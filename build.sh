2ef3e98082839ff0d1c04f273a43852ace1c9ea3
Sergey Berezansky <sergey@weka.io>
2025-02-19 13:23:25 +0200

    feat: add darwin/arm64 build

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gox -osarch="darwin/amd64 linux/386 linux/amd64 windows/amd64 freebsd/386 freebsd/amd64 linux/arm linux/arm64" --output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}"
+gox -osarch="darwin/amd64 darwin/arm64 linux/386 linux/amd64 windows/amd64 freebsd/386 freebsd/amd64 linux/arm linux/arm64" --output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}"
